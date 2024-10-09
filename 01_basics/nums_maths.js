0cc6d8f51de5807e4ebb5e683cadb9af5fa93434
Anant Saraswat <116835722+ANANTSARASWAT30@users.noreply.github.com>
2024-10-09 19:35:55 +0000

    math_num

---
+++ 01_basics/nums_maths.js
@@ -0,0 +1,42 @@
+const num=400
+
+console.log(num)
+
+const num1=new n=Number(100)
+
+console.log(num1)
+
+console.log(num1.toString().length)
+
+const num3=23.33566
+console.log(num1.toFixed(1))
+
+console.log(num3.toPrecision(3))
+
+const num4=10000000
+console.log(num4.toLocaleString(en-IN))
+
+
+// Mathematics
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4.2))
+
+console.log(Math.max(4,3,6,8))
+
+console.log(Math.random())
+
+console.log((Math.random()*10) + 1)
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random() * (max-min+1)) + min)
+
+
+ 
+
+
---
+++ 01_basics/strings.js
@@ -0,0 +1,42 @@
+ const name="Anant"
+ const repoCount=50
+
+ // console.log(name+repoCount+"Value")
+ 
+ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const str=new String("Anant")
+
+console.log(str[0])
+
+console.log(str.length)
+console.log(str.toUpperCase())
+console.log(str.charAt(2))
+console.log(str.indexOf('t'))
+
+ const str2=str.substring(0,3)
+ 
+ console.log(str2)
+
+const str3="    Anant    "
+console.log(str3);
+console.log(str3.trim())
+
+const url="https://anant.com/anant%30saraswat"
+
+console.log(url.replace('%30','-'))
+
+url.includes('anant')
+
+const str4=("Anant-saraswat-Mathura")
+
+console.log(str4.split('-'))
+
+
+
+
+
+
+
+
+
