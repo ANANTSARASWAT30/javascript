a8e30c461d6390f23ae4bf8bee742cfcb3075333
Anant Saraswat <116835722+ANANTSARASWAT30@users.noreply.github.com>
2024-10-09 13:55:54 +0000

    change in location of file

---
+++ 01_basics/BTN.js
@@ -0,0 +1,13 @@
+  let str1="Hello"
+  let str2=" World"
+  let str3=str1+str2
+
+  console.log(str3)
+
+  console.log("1"+2)
+  console.log(1+"2")
+  console.log("1"+2+2)
+  console.log(1+2+"2")
+  console.log(+true)
+  console.log(+"")
+  
---
+++ 01_basics/datatype_summary.js
@@ -0,0 +1,28 @@
+//primitive (call by value)
+// 7 types:String, Number, Boolean, null ,undefined, Symbol, BigInt
+
+
+//Non primitive (call by Reference)
+
+//Array, Objects, Functions
+
+//JS is dynamicalyy typed
+
+const score=100
+const scorev=100.3
+
+const id=Symbol('123')
+const id1=Symbol('123')
+
+const heros=["shaktiman","naagraj","doga"]
+
+let myObj={
+    name:"Anant",
+    age:22
+}
+
+const myFunc=function(){
+    console.log("Hello World")
+}
+
+console.log(typeof myObj)
