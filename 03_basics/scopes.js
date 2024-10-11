042e8158f5d3ab27f2768008737ade7dbc9b0edd
Anant Saraswat <116835722+ANANTSARASWAT30@users.noreply.github.com>
2024-10-11 20:37:19 +0000

    scopes

---
+++ 03_basics/scopes.js
@@ -0,0 +1,44 @@
+
+
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+function one(){
+    const username="Anant"
+
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+
+    // console.log(website)
+    two()
+
+
+}
+
+one()
+
+// interesting
+console.log(addone(5))
+function addone(num){
+    return num+1
+}
+
+
+addTwo(5)
+const addTwo= function(num){
+    return num+2
+}
+
+addTwo(5)
+
+
+
